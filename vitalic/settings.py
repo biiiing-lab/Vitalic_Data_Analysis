d5a2018164f89a1930b4963f762f8df1757e176d
bing <99920799+biiiing-lab@users.noreply.github.com>
2024-10-10 16:45:57 +0900

    Update settings.py

--- vitalic/settings.py
+++ vitalic/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vitalic',
         'USER': 'root',
-        'PASSWORD': 'wkddbqls0901',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
