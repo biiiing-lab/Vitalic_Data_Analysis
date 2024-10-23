d0a60f3c9e9b23b7376098e75df49610c8853cf6
jangy <jangyb0901@gmail.com>
2024-10-23 19:17:34 +0900

    fix : url 수정 feat : url 추가 - 고정 지출 패턴 분석

--- AnalysisApp/AnalysisApp/urls.py
+++ AnalysisApp/AnalysisApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     # monthly, weekly, daily = mwd
-    path('api/mwd/report', views.transaction_summary, name='transaction_summary'),
+    path('api/report/mwd', views.transaction_summary, name='transaction_summary'),
+    path('api/report/fixed', views.fixed_expenses, name='fixed_expenses'),
 ]
