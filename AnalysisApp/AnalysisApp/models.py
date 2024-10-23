cab26f36b6d5a122192efc7f68d4baddf1f95bf3
jangy <jangyb0901@gmail.com>
2024-10-23 15:52:34 +0900

    fix : max 기본값 지정

--- AnalysisApp/AnalysisApp/models.py
+++ AnalysisApp/AnalysisApp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class passbook (models.Model) :
-    bank_name = models.CharField()
-    account_number = models.CharField()
+    bank_name = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=100)
     balance_amt = models.IntegerField()
     inout_type = models.IntegerField()  # 0: 입금, 1: 출금
-    in_des = models.CharField()
-    out_des = models.CharField()
+    in_des = models.CharField(max_length=100)
+    out_des = models.CharField(max_length=100)
     tran_date_time = models.DateTimeField()
     tran_type = models.IntegerField()
     tran_amt = models.IntegerField()
