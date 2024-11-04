cd9518d794cfa9158e13e425c8fd5e31ecc0cc01
jangy <jangyb0901@gmail.com>
2024-11-04 23:42:53 +0900

    feat : 시각화 email 발송 url

--- AnalysisApp/AnalysisApp/urls.py
+++ AnalysisApp/AnalysisApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/report/calendar', views.calendar_return, name='calendar_return'),
     path('api/report/monthly', views.monthly_summary, name='monthly_summary'),
     path('api/report/calendar/all', views.monthly_return, name='quarter_summary'),
+    path('api/report/visualization', views.visualization_pdf, name='visualization_pdf'),
 ]
